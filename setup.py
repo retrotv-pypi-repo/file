0d84caf127bc5200e4687b8738d75f687164e809
yjj8353 <yjj8353@gmail.com>
2024-11-25 13:42:18 +0900

    자동배포 준비

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="retrotv-file",
+    version="0.0.2-alpha",
+    description="파일을 편하게 다루기 위한 패키지",
+    author="RetroTV",
+    author_email="yjj8353@gmail.com",
+    url="https://github.com/retrotv-pypi-repo/file",
+    install_requires=[],
+    packages=find_packages(exclude=[]),
+    keywords=["retrotv", "RetroTV", "yjj8353", "file", "File", "pypi"],
+    python_requires=">=3.8",
+    package_data={},
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+    ],
+)
